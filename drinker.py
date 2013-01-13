4ca7752cc5e0a60d0f572f7089b01c784bcf89af
Matthew Jaskula <mjaskula@gmail.com>
2013-01-13 00:41:19 -0500

    data from breweryDB lib is now json already, switched to simplejson, added comments

--- drinker.py
+++ drinker.py
@@ -1,18 +1,24 @@
-import os,json
+import os
+import simplejson as json
 from brewerydb import *
 
 OUTPUT = "output"
+
+# setup
 
 if not os.path.exists(OUTPUT):
     os.makedirs(OUTPUT)
 
 BreweryDb.configure(open("apikey").readline().strip())
 
+print "Loading styles..."
 
-styles = json.loads(BreweryDb.styles())
+styles = BreweryDb.styles()
 
 with open(OUTPUT + '/styles.json', 'w') as outfile:
   json.dump(styles, outfile)
 
+
+# load and write the beers
 
 
