8db9cb7a2336c26ab4d4129c7e64d05bfe8d7ba5
Matthew Jaskula <mjaskula@gmail.com>
2013-01-13 16:26:34 -0500

    broke it up into functions

--- drinker.py
+++ drinker.py
@@ -1,24 +1,44 @@
-import os
+import os, sys
 import simplejson as json
 from brewerydb import *
 
 OUTPUT = "output"
 
-# setup
 
-if not os.path.exists(OUTPUT):
-    os.makedirs(OUTPUT)
-
-BreweryDb.configure(open("apikey").readline().strip())
-
-print "Loading styles..."
-
-styles = BreweryDb.styles()
-
-with open(OUTPUT + '/styles.json', 'w') as outfile:
-  json.dump(styles, outfile)
+def main():
+	ensureOutputDir()
+	configure()
+	loadBeers(loadStyles())
 
 
-# load and write the beers
+def ensureOutputDir():
+	if not os.path.exists(OUTPUT):
+	    os.makedirs(OUTPUT)
 
 
+def configure():
+	BreweryDb.configure(open("apikey").readline().strip())
+
+
+def loadStyles():
+	print "Loading styles..."
+	return writeToJson(BreweryDb.styles(), 'styles.json')
+
+
+def loadBeers(styles):
+	for style in styles['data']:
+		processBeersForStyle(style)
+		
+
+def processBeersForStyle(style):
+	print 'Loading beers for style {0} {1}...'.format(style['id'], style['name'].encode(sys.stdout.encoding, 'replace'))
+	print BreweryDb.beers({'styleId': style['id']})
+	sys.exit(0)
+
+def writeToJson(object, filename):
+	with open(OUTPUT + '/' + filename, 'w') as outfile:
+	  json.dump(object, outfile)
+	return object
+
+if __name__ == "__main__":
+    main()
