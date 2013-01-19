77896d05cc1996929bea75ac40a5513439803fce
Matthew Jaskula <mjaskula@gmail.com>
2013-01-19 15:07:57 -0500

    prints out the names and breweries for the first page of beers from the first style

--- drinker.py
+++ drinker.py
@@ -1,4 +1,5 @@
 import os, sys
+import pprint
 import simplejson as json
 from brewerydb import *
 
@@ -22,18 +23,34 @@ def configure():
 
 def loadStyles():
 	print "Loading styles..."
-	return writeToJson(BreweryDb.styles(), 'styles.json')
+	styles = handleBreweryDbResponse(BreweryDb.styles())
+	return writeToJson(styles, 'styles.json')
 
 
 def loadBeers(styles):
-	for style in styles['data']:
-		processBeersForStyle(style)
+	#for style in styles:
+		loadBeersForStyle(styles[0])
 		
 
-def processBeersForStyle(style):
+def loadBeersForStyle(style):
 	print 'Loading beers for style {0} {1}...'.format(style['id'], style['name'].encode(sys.stdout.encoding, 'replace'))
-	print BreweryDb.beers({'styleId': style['id']})
-	sys.exit(0)
+	beers = handleBreweryDbResponse(BreweryDb.beers({'styleId': style['id'], 'withBreweries': 'Y'}))  #TODO: handle pagination
+	for beer in beers:
+		processBeer(beer)
+
+def processBeer(beer):
+	#pprint.pprint(beer)
+	print '{0} - {1}'.format(beer['breweries'][0]['name'].encode(sys.stdout.encoding, 'replace'), beer['name'].encode(sys.stdout.encoding, 'replace'))
+
+
+# helpers
+
+def handleBreweryDbResponse(response):
+	if (response['status'] == "success"):
+		return response['data']
+	else:
+		print response
+		sys.exit(1)
 
 def writeToJson(object, filename):
 	with open(OUTPUT + '/' + filename, 'w') as outfile:
